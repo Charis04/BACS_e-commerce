a6553e81bd01766f0705b643bb4277c2c6464d97
scodeq65 <sodiqagbaraojo@gmail.com>
2024-12-18 14:57:29 +0100

    Implemennted user management system

--- app.py
+++ app.py
@@ -1,19 +1,22 @@
 from flask import Flask
+from flask_jwt_extended import JWTManager
 from shophive_packages import db
 from flask_migrate import Migrate
 import os
 
 app = Flask(__name__)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+app.config['JWT_SECRET_KEY'] = os.getenv('SECRET_KEY')  # Add JWT_SECRET_KEY
 
 db.init_app(app)
+jwt = JWTManager(app)
 migrate = Migrate(app, db)
 
-# Debug registered tables
-with app.app_context():
-    from shophive_packages.models import User, Product
-    print(f"Registered tables: {db.metadata.tables.keys()}")
+# Register routes
+from shophive_packages.routes.user_routes import *
 
 if __name__ == '__main__':
     app.run(debug=True)
