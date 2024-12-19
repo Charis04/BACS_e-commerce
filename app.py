e1dd5b4afdbcc4ee60efe43b50c6767acf858e62
Francis Waihiga <francisgwaihiga@gmail.com>
2024-12-19 19:27:04 +0300

    registered blueprint for the new product api and tracked it running with a for loop

--- app.py
+++ app.py
@@ -2,8 +2,14 @@ from flask import Flask
 from shophive_packages import db
 from flask_migrate import Migrate
 import os
+from dotenv import load_dotenv
+from product_management_routes.new_product_routes import new_product_bp
+
+# load environment variables
+load_dotenv()
 
 app = Flask(__name__)
+app.register_blueprint(new_product_bp)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
@@ -15,5 +21,10 @@ with app.app_context():
     from shophive_packages.models import User, Product
     print(f"Registered tables: {db.metadata.tables.keys()}")
 
+
+for rule in app.url_map.iter_rules():
+    print(f"{rule} -> {rule.methods}")
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="127.0.0.1", port=5003, debug=True)
