aef7f7324eda87f88a28bd0620de9b98dd31bb93
Francis Waihiga <francisgwaihiga@gmail.com>
2024-12-19 19:28:49 +0300

    Added a created at and updated at for the new product

--- shophive_packages/models/product.py
+++ shophive_packages/models/product.py
@@ -1,10 +1,15 @@
 from shophive_packages import db
+from sqlalchemy.sql import func
+
 
 class Product(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(100), nullable=False)
     description = db.Column(db.String(200), nullable=False)
     price = db.Column(db.Float(), nullable=False)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(
+        db.DateTime, server_default=db.func.now(), onupdate=db.func.now())
 
     def __repr__(self):
         return f'<Product {self.name}>'
