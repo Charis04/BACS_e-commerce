5c8ce0358a984ee388ad7849086e800d954eb773
charis <charisadu@gmail.com>
2025-01-22 23:34:09 +0100

    half commit

--- config.py
+++ config.py
@@ -31,10 +31,12 @@ class DevelopmentConfig(Config):
     """
     Development configuration class.
     """
+    """
     DB_USER = os.getenv('DB_USER', 'postgres')
     DB_PASS = os.getenv('DB_PASS', 'postgres')
     DB_NAME = os.getenv('DB_NAME', 'shophive')
     DB_HOST = os.getenv('DB_HOST', 'localhost')
+    """
 
     SQLALCHEMY_DATABASE_URI = (
         f"postgresql://{DB_USER}:{DB_PASS}"
